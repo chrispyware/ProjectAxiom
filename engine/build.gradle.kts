32ee7403fb672e0d6e65223d8469c5debfdf73e7
chrispyware <thegreatchrispy@gmail.com>
2026-08-11 19:36:41 -0500

    configure version catalog and JUnit

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -8,7 +8,17 @@ java {
     }
 }
 
+dependencies {
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
 tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
     options.release = 21
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
--- game/build.gradle.kts
+++ game/build.gradle.kts
@@ -10,9 +10,17 @@ java {
 
 dependencies {
     implementation(project(":engine"))
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
     options.release = 21
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
